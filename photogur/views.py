dc3cb947f494878bf6f202d0f1da848f6c1541d8
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-03-04 13:51:49 -0500

    added pages for individual pictures

--- photogur/views.py
+++ photogur/views.py
@@ -1,12 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from photogur.models import Picture, Comment
-import ipdb
+from photogur.models import Picture  # , Comment
+# import ipdb
+
 
 def pictures_page(request):
     pictures = Picture.objects.all()
     context = {'name': 'Jeff', 'pictures': pictures}
-    print(pictures)
+    # print(pictures)
     # print(len(pictures[0].comments.all()))
     response = render(request, 'pictures.html', context)
+    return HttpResponse(response)
+
+
+def picture_show(request, id):
+    picture = Picture.objects.get(pk=id)
+    context = {'picture': picture}
+    response = render(request, 'picture.html', context)
     return HttpResponse(response)
