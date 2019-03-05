02fbdff272952ca76ebb3279bc5025f368c51b25
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-03-05 15:57:25 -0500

    styled the search results page, still need to style the individual pages

--- photogur/views.py
+++ photogur/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from photogur.models import Picture  # , Comment
+from photogur.models import Picture, Comment
 # import ipdb
+# import pry
 
 
 def pictures_page(request):
@@ -22,7 +23,20 @@ def picture_show(request, id):
 
 def picture_search(request):
     query = request.GET['query']
-    search_results = Picture.objects.filter(artist=query)
+    search_results = Picture.objects.filter(artist__contains=query) | Picture.objects.filter(title__contains=query) | Picture.objects.filter(url__contains=query)
+    # search_results = Picture.objects.filter(artist=query) | Picture.objects.filter(title=query) | Picture.objects.filter(url=query)
+    # ipdb.set_trace()
+    # pry()
     context = {'pictures': search_results, 'query': query}
     response = render(request, 'search_results.html', context)
     return HttpResponse(response)
+
+
+def create_comment(request):
+    comment_name = request.POST['comment_name']
+    comment_message = request.POST['comment_message']
+    comment_picture = Picture.objects.get(pk=request.POST['picture'])
+    new_comment = Comment.objects.create(name=comment_name,
+                                         message=comment_message,
+                                         picture=comment_picture)
+    return HttpResponseRedirect('/picture/' + request.POST['picture'])
